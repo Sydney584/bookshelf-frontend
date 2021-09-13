1110a4c1b621c854dbaa69fb2de588231c2266f1
sydney <sydneychase5841@gmail.com>
2021-09-13 18:39:37 -0400

    checking frontend w/static data

---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="content">
+            <h1>About</h1>
+            <p>This app allows you to browse through recommended books we have read on The Indigo Room Podcast and YouTube show</p>
+
+        <p>To enter the site please register below and create a username to login</p>
+        </div>
+        </div>
+    );
+}
+ 
+export default Home;
